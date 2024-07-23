89003af8896e40122db5a9fb1a4735538b0c2d4c
RobinRehmers <robinrehmers@gmx.de>
2024-07-23 18:04:49 +0200

    added Sqlitedataaccess

--- RetailCommanderLibrary/Database/ISqliteDataAccess.cs
+++ RetailCommanderLibrary/Database/ISqliteDataAccess.cs
@@ -6,16 +6,9 @@ using System.Threading.Tasks;
 
 namespace RetailCommanderLibrary.Database
 {
-    public interface ISqlDataAccess
+    public interface ISqliteDataAccess
     {
-        List<T> LoadData<T, U>(string sqlStatement,
-                        U parameters,
-                        string connectionStringName,
-                        bool isStoredProcedure = false);
-
-        void SaveData<T>(string sqlStatement,
-                        T parameters,
-                        string connectionStringName,
-                        bool isStoredProcedure = false);
+        List<T> LoadData<T, U>(string sqlStatement, U parameters, string connectionStringName);
+        void SaveData<T>(string sqlStatement, T parameters, string connectionStringName);
     }
 }
--- RetailCommanderLibrary/Database/SqliteDataAccess.cs
+++ RetailCommanderLibrary/Database/SqliteDataAccess.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SQLite;
+using Dapper;
 
 namespace RetailCommanderLibrary.Database
 {
-    internal class SqliteDataAccess
+    public class SqliteDataAccess : ISqliteDataAccess
     {
+
+        private readonly IConfiguration _config;
+
+        public SqliteDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public List<T> LoadData<T, U>(string sqlStatement,
+            U parameters,
+            string connectionStringName)
+        {
+            string connectionString = _config.GetConnectionString(connectionStringName);
+
+            using (IDbConnection connection = new SQLiteConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(sqlStatement, parameters).ToList();
+                return rows;
+            }
+        }
+
+        public void SaveData<T>(string sqlStatement,
+            T parameters,
+            string connectionStringName)
+        {
+            string connectionString = _config.GetConnectionString(connectionStringName);
+
+            using (IDbConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Execute(sqlStatement, parameters);
+            }
+        }
     }
 }
