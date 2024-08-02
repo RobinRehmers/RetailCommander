9336ed02ef8267bc40f8e4d8ba435bcdbf94d6e6
RobinRehmers <robinrehmers@gmx.de>
2024-08-02 22:00:09 +0200

    added monthlysalesmodel

---
+++ RetailCommanderLibrary/Models/MonthlySalesModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailCommanderLibrary.Models
+{
+    public class MonthlySalesModel
+    {
+        public int Id { get; set; }
+        public string Month { get; set; }
+        public double Sales { get; set; }
+    }
+}
